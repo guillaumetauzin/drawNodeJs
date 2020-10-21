a7edc9e9b1e4746f2a08fcbc590fa4c81d7af7fd
Guillaume <36336159+guillaumetauzin@users.noreply.github.com>
2020-10-21 19:48:06 +0200

    Update

---
+++ main.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+var count = 0;
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+	
+	count++;
+  	socket.broadcast.emit('counter', count);
+  	socket.emit('counter', count);
+
+	socket.on('sendCursors', function (data) {
+        console.log(data);
+        socket.broadcast.emit('sendPoints', data);
+        socket.emit('sendPoints', data);
+    });
+
+    socket.on('sendClear', function(data2) {
+        if (data2 == "azaz") {
+        	console.log("Ecran Reset !")
+    		socket.broadcast.emit('clearScreen');
+    		socket.emit('clearScreen');
+        }
+
+    });
+
+    socket.on('disconnect', function() {
+	    count--;
+	    socket.broadcast.emit('counter', count);
+  	});
+
+});
+
+
+
+server.listen(8080);
+
+console.log('Serveur lancé sur le port 8080 !');
